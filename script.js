3479d5b12af4d46dacacc7383cd7f38ea610586e
ali1215311 <mohammad.ali1215311@gmail.com>
2026-04-16 20:23:22 +0600

    Add console log for testing rebase

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const colors = ["#ffffff", "#e3f2fd", "#f1f8e9", "#fff3e0", "#fce4ec"];
 let colorIndex = 0;
 
 console.log("Hola from github");
+console.log("Another Edit for testing rebase");
 
 btn.addEventListener("click", () => {
   colorIndex = (colorIndex + 1) % colors.length;
